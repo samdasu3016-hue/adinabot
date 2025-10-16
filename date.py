f15498a03621003888998e01364ee0b10d701d4f
samdasu3016-hue <samdasu3016@gmail.com>
2025-10-16 23:30:42 +0900

    Refactor attendanceBot for improved functionality

--- date.py
+++ date.py
@@ -58,4 +58,5 @@ async def 출석(interaction: discord.Interaction):
     await interaction.response.send_message(f"{user.mention}, 오늘도 안녕하다요!\n (누적 {user_data['count']}일차/연속 {user_data['streak']}일차)")
 
 
-bot.run('MTQyNzcwMDI0NTQyNDM3Mzg0MQ.GJ1io0.5z17ip1F_MxyIKpSmpNStMsKTeDoyC5-jbWg5E') # 실제 봇 토큰임 이거유출되면좃됨
+bot.run('MTQyODM4NDMxNTg5MjU2ODEzNA.GBJ-xc.ruGTRF5bov2Dm1nQ1hzwnWy5cfacK_4Ds4X9nE') # 실제 봇 토큰임 이거유출되면좃됨
+
