457cf6d61f811098b745ddbe11f3aa62fc1db2ce
samdasu3016-hue <samdasu3016@gmail.com>
2025-10-17 00:01:19 +0900

    Update date.py

--- date.py
+++ date.py
@@ -61,5 +61,6 @@ async def 출석(interaction: discord.Interaction):
     await interaction.response.send_message(f"{user.mention}, 오늘도 안녕하다요!\n (누적 {user_data['count']}일차/연속 {user_data['streak']}일차)")
 
 
-bot.run(TOKEN)# 실제 봇 토큰임 이거유출되면좃됨
+bot.run(TOKEN)
 print("TOKEN:", TOKEN)
+
